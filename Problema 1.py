caf3464a1a94a891e0db044c982522f46991bbb2
Gabriel Tito <joaogtito@gmail.com>
2024-04-17 10:14:29 -0300

    updt

--- Problema 1.py
+++ Problema 1.py
@@ -9,35 +9,30 @@ i = 0
 for i in range(0, 25):
     i += 1
     intervalo1.append(i)
-print(intervalo1)
 
 intervalo2 = []
 z = 0
 for z in range(25, 50):
     z += 1
     intervalo2.append(z)
-print(intervalo2)
 
 intervalo3 = []
 f = 0
 for f in range(50, 75):
     f += 1
     intervalo3.append(f)
-print(intervalo3)
 
 intervalo4 = []
 h = 0
 for h in range(75, 100):
     h += 1
     intervalo4.append(h)
-print(intervalo4)
 
 x = 1
 #Repetição
 while x >= 0:
-    
     x = int(input("Digite um número inteiro positivo para iniciar, e um número negativo para parar: \n"))
-    if x <= 25:
+    if x <= 25 and x > 0:
         cont1 += 1
     if x >= 26 and x <= 50:
         cont2 += 1
@@ -45,9 +40,5 @@ while x >= 0:
         cont3 += 1
     if x >= 76 and x <= 100:
         cont4 += 1
-print("A quantidade de números no primeiro intervalo:", cont1)
-print("A quantidade de números no segundo intervalo:", cont2)
-print("A quantidade de números no terceiro intervalo:", cont3)
-print("A quantiadde de números no Quarto intervalo:", cont4)
-    
-
+#Resultados
+print("A quantidade de números no primeiro intervalo:", cont1, "A quantidade de números no segundo intervalo:", cont2,"A quantidade de números no terceiro intervalo:", cont3, "A quantiadde de números no Quarto intervalo:", cont4 )
