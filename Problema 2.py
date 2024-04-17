983bcf4b080efba383fe2de7ea9f2aafcff279f2
Gabriel Tito <joaogtito@gmail.com>
2024-04-17 08:54:55 -0300

    update

--- Problema 2.py
+++ Problema 2.py
@@ -1,9 +1,6 @@
-item1 = 1.2
-item2 = 1.3
-item3 = 1.5
-item4 = 1.2
-item5 = 1.3
-item6 = 1.0
+qtItems = [0, 0, 0, 0, 0, 0]
+lista = [1.2, 1.3, 1.5, 1.2, 1.3, 1]
+lista2 = [0, 0, 0, 0, 0, 0]
 
 pedido = True
 
@@ -21,38 +18,44 @@ Refrigerante -- 106 -- R$1,0
 
 print(Mensagem)
 
-
 preço_total = 0
 while pedido == True:
     cod = int(input("Informe o código do item desejado: \n"))
+    qt = int(input("Quantos itens você deseja?: \n"))
     
     if cod > 106 and cod < 101:
         print("Item não existente")
     
     if cod == 101:
-        qt = int(input("Quantos itens você deseja?: \n"))
-        item1 += 1
+        qtItems[0] += 1
+        lista2[0] = lista[0] * 1.2
         preço_total += 1.2 * qt
+        
     elif cod == 102:
-        qt = int(input("Quantos itens você deseja?: \n"))
-        item2 += 1
+        qtItems[1] += 1
+        lista2[1] = lista[1] * 1.3
         preço_total += 1.3 * qt
+        
     elif cod == 103:
-        qt = int(input("Quantos itens você deseja?: \n"))
-        cod += 1
+        qtItems[2] += 1
+        lista2[2] = lista[2] * 1.5
         preço_total += 1.5 * qt
-    elif item3 == 104:
-        qt = int(input("Quantos itens você deseja?: \n"))
-        item4 += 1
+        
+    elif cod == 104:
+        qtItems[3] += 1
+        lista2[3] = lista[3] * 1.2
         preço_total += 1.2 * qt
+        
     elif cod == 105:
-        qt = int(input("Quantos itens você deseja?: \n"))
-        item5 += 1
+        qtItems[4] += 1
+        lista2[4] = lista[4] * 1.3
         preço_total += 1.3 * qt
+        
     elif cod == 106:
-        qt = int(input("Quantos itens você deseja?: \n"))
-        item6 += 1
+        qtItems[5] += 1
+        lista2[5] = lista[5] 
         preço_total += 1 * qt
+        
         
     else:
          print("Item não existente")
@@ -62,6 +65,8 @@ while pedido == True:
         pedido = True
     elif pedido == "N":
         continue
-print("O valor total da compra foi de:",preço_total)
     
+    
+print("O valor total da compra foi de:",preço_total)
+print("O valor de cada item foi de:")
     
